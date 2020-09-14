82a2235eed71e108214adeae509c4fbfd5fe80aa
Jesus LOVON <jlovon@newmail.lip6.fr>
2020-09-14 09:52:07 +0200

    Using finetuned bm25 for microblog

--- onir/bin/catfog.py
+++ onir/bin/catfog.py
@@ -17,18 +17,18 @@ models_ranker = {
 }
 
 models_gpu = {
-	"drmm":"2",
-	"pacrr":"0",
-	"knrm":"2",
-	"vbert":"1",
-	"cedr":"1"
+	"drmm":"1",
+	"pacrr":"2",
+	"knrm":"0",
+	"vbert":"3",
+	"cedr":"3"
 }
 
 
 datasets_training = {
 	"msmarco":"msmarco_train_bm25_k1-0.82_b-0.68.100_mspairs",
 	"cord19":"covid_trf2-rnd5-quest_bm25_k1-3.9_b-0.55.1000_2020-07-16_bs-text_2020filter_bsoverride-rnd5-query_rr-title_abs",
-        "microblog":"microblog_train_bm25.100"
+        "microblog":"microblog_train_bm25_k1-0.2_b-0.95.100"
 }
 
 config_dataset = {
